1438c43c13dff4cda49e57bcb9ea7e507b27c6c5
mvime <mvi@mvi.me>
2013-03-02 19:56:14 +0100

    Adds the option to enter tags as an optional parameter. Displaying tags and searching tags not in yet.

--- life_logger.py
+++ life_logger.py
@@ -6,7 +6,7 @@
 # BackwardsReader implementation: Copyright (c) Peter Astrand <astrand@cendio.se>, PSF License
 
 import sys, datetime, os, string
-#Syntax: python life_logger.py "Text to log" start:-5h ;; start:is an optional parameter
+#Syntax: python life_logger.py "Text to log" @tag start:-5h ;; start: and @tag are optional parameters
 
 # We set the timestamp
 timestamp = datetime.datetime.now()
@@ -16,8 +16,9 @@ default_start = timestamp - datetime.timedelta(hours = 1)
 
 # We define the LogEntry class, which needs an action, an end_time (timestamp) and a start_time (set by user or default_start)
 class LogEntry:
-	def __init__(self, action, start_time = default_start, end_time = timestamp):
+	def __init__(self, action, tags, start_time = default_start, end_time = timestamp):
 		self.action = action
+		self.tags = tags
 		self.end = end_time
 		self.start = start_time
 
@@ -90,40 +91,47 @@ def read_backwards(file, lines, *args): # args[0]:since, args[1]:up-to
 				i = i+1
 		return out
 	except IOError:
-		return "Error";
+		return False;
 
 
 def log_displayer(results):
 	for r in results:
-		print r[:-1]
+		print r[:-1] # The last two characters are stripped because they are "\n" and force a new line
 
 
 
 def log_maker(user_input): # Processes user input and extracts a log or returns an error.
 	error = None
 	log = None
+	tags = []
+	new_start = None
 
-	if len(user_input) == 3: #life_logger.py  + "Text" + start:
-		#user_input[2] is the optional argument where we set the start time
-		time_amount = float(user_input[2][7:-1]) #syntax is "start:-XXu", "start:-" is 7 chars long, so the amount starts at the 8th
-		#stime_unit = user_input[2][-1:] #units are set with only one character: s, h, d, y
-		start_time = timestamp - datetime.timedelta(hours = time_amount)
-		log = LogEntry(user_input[1],start_time)
-
+	if len(user_input) == 1:
+		error = 'At the very least you need to enter one action. Please use the following syntax: \'python life_logger.py "Text to log" start:-5h ;; start:is an optional parameter\''
+		
 	elif len(user_input) == 2: #life_logger.py  + "Text"
 		prev_entry = str(read_backwards('my_life.txt', 1))
-		if prev_entry == "Error":
-			log = LogEntry(user_input[1])
-		else:	
+		if prev_entry:
 			start_finished = prev_entry.find('Finished: ')
 			end_finished = prev_entry.find(' |', start_finished)
-			prev_entry_finished = datetime.datetime.strptime(prev_entry[start_finished+10:end_finished], '%Y-%m-%d %H:%M')
-			log = LogEntry(user_input[1], prev_entry_finished)
-
-	elif len(user_input) == 1:
-		error = 'At the very least you need to enter an action. Please use the following syntax: \'python life_logger.py "Text to log" start:-5h ;; start:is an optional parameter\''
+			new_start = datetime.datetime.strptime(prev_entry[start_finished+10:end_finished], '%Y-%m-%d %H:%M')
+	
+	else: #life_logger.py  + "Text" + any or all of the optional parameters
+		assert len(user_input) >= 3
+		for i in user_input[2:]:
+			if '@' in i:
+				tags.append(i)
+			if 'start:' in i:
+				time_amount = float(i[7:-1]) #syntax is "start:-XXu", "start:-" is 7 chars long, so the amount starts at the 8th
+				#stime_unit = i[-1:] #units are set with only one character: s, h, d, y
+				new_start = timestamp - datetime.timedelta(hours = time_amount)			
+		
+	if new_start:
+		log = LogEntry(user_input[1], tags, new_start)
 	else:
-		error = 'Too many parameters'
+		log = LogEntry(user_input[1], tags)
+
+	
 	return log, error
 
 
@@ -139,14 +147,15 @@ def decision_maker(user_input): # Processes user input and determines whether s/
 		log_displayer(read_backwards('my_life.txt', lines))
 
 	else:
-		maker_result = log_maker(sys.argv)
+		maker_result = log_maker(user_input)
 		if maker_result[1] != None:
 			print maker_result[1] #print the error
 		else:
 			assert maker_result[0] != None
 			log = maker_result[0]
 			elapsed = log.end - log.start
-			entry = '"' + log.action + '" | Started: ' + str(log.start)[:16] + ' | Finished: ' + str(log.end)[:16] + ' | Elapsed: ' + str(elapsed)[:4] + ' |\n'
+			tags = ', '.join(log.tags)
+			entry = '"' + log.action + '" | Started: ' + str(log.start)[:16] + ' | Finished: ' + str(log.end)[:16] + ' | Elapsed: ' + str(elapsed)[:4] + ' | Tags: ' + tags + ' |\n'
 			append_to('my_life.txt', entry)
 			print entry
 
